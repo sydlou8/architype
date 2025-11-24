042ded35ac6a5e03689f8853397ba4467ef668ce
Sydney Morano <syd.morano@gmail.com>
2025-11-23 21:36:56 -0800

    fixed imports

--- app/models/game/constants.py
+++ app/models/game/constants.py
@@ -1,10 +1,10 @@
-from app.models.game.party import Party
+from models.game.party import Party
 
-from app.models.game.character.classes.barista import Barista
-from app.models.game.character.classes.biker import Biker
-from app.models.game.character.classes.drag_queen import DragQueen
-from app.models.game.character.classes.emo import Emo
-from app.models.game.character.classes.jock import Jock
+from models.game.entities.character.barista import Barista
+from models.game.entities.character.biker import Biker
+from models.game.entities.character.drag_queen import DragQueen
+from models.game.entities.character.emo import Emo
+from models.game.entities.character.jock import Jock
 
 # Default Party for New Users --> Tutorial Purpose
 # Player will start with these 5 characters and they can later customize their party.
